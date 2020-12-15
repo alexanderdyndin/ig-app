581851055e50592ff1aef0084c966e72bd754d7d
Alexander <like81@yandex.ru>
2020-12-15 18:35:56 +0300

    appodeal entity, hided deprecation

--- app/src/main/java/com/intergroupapplication/domain/entity/AdEntity.kt
+++ app/src/main/java/com/intergroupapplication/domain/entity/AdEntity.kt
@@ -1,5 +1,11 @@
 package com.intergroupapplication.domain.entity
 
-data class AdEntity (val limitOfAdsGroups: Int, val FirstAdIndexGroups: Int,
-                     val noOfDataBetweenAdsGroups: Int, val limitOfAdsNews: Int,
-                     val FirstAdIndexNews: Int, val noOfDataBetweenAdsNews: Int)
+data class AdEntity (val limitOfAdsGroups: Int,
+                     val FirstAdIndexGroups: Int,
+                     val noOfDataBetweenAdsGroups: Int,
+                     val limitOfAdsNews: Int,
+                     val FirstAdIndexNews: Int,
+                     val noOfDataBetweenAdsNews: Int,
+                     val limitOfAdsComments: Int,
+                     val FirstAdIndexComments: Int,
+                     val noOfDataBetweenAdsComments: Int)
