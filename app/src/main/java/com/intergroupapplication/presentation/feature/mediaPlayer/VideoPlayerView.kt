1122aae35e85161f4f83ad68398424207050c8bd
Artur Garifullin <rus_artur4ik@mail.ru>
2021-02-23 02:09:40 +0300

    audio service also works with video in background. Some improvements in audio playback, bugfixes

--- app/src/main/java/com/intergroupapplication/presentation/feature/mediaPlayer/VideoPlayerView.kt
+++ app/src/main/java/com/intergroupapplication/presentation/feature/mediaPlayer/VideoPlayerView.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.intergroupapplication.R
 
-class AudioPlayerView @JvmOverloads constructor(
+class VideoPlayerView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
-    private val _exoPlayer = LayoutInflater.from(context).inflate(R.layout.view_music_player, this)
-    val exoPlayer by lazy<PlayerView> { _exoPlayer.findViewById(R.id.musicExoPlayerView) }
+    private val _exoPlayer = LayoutInflater.from(context).inflate(R.layout.view_video_player, this)
+    val exoPlayer by lazy<StyledPlayerView> { _exoPlayer.findViewById(R.id.videoExoPlayerView) }
 
     init {
         this.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
