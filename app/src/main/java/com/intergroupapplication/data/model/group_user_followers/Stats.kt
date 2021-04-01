ed268c628532a3c09be0c1aa2ec43b2aceeb20e3
arov-anvar <43091339+arov-anvar@users.noreply.github.com>
2021-04-01 21:11:41 +0300

    delete unnecessary serialized name

--- app/src/main/java/com/intergroupapplication/data/model/group_user_followers/Stats.kt
+++ app/src/main/java/com/intergroupapplication/data/model/group_user_followers/Stats.kt
@@ -1,15 +1,9 @@
 package com.intergroupapplication.data.model.group_user_followers
 
 
-import com.google.gson.annotations.SerializedName
-
 data class Stats(
-    @SerializedName("comments")
     val comments: Int,
-    @SerializedName("dislikes")
     val dislikes: Int,
-    @SerializedName("likes")
     val likes: Int,
-    @SerializedName("posts")
     val posts: Int
 )
