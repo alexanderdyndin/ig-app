4456736508e59785d8d6a4cc1bb5076ef21c93d7
arov-anvar <43091339+arov-anvar@users.noreply.github.com>
2021-05-26 08:51:15 +0300

    init

---
+++ app/src/main/java/com/intergroupapplication/data/db/dao/PostDao.kt
@@ -0,0 +1,7 @@
+package com.intergroupapplication.data.db.dao
+
+import androidx.room.Dao
+
+@Dao
+interface PostDao {
+}
---
+++ app/src/main/java/com/intergroupapplication/data/db/entity/GroupPostModel.kt
@@ -0,0 +1,9 @@
+package com.intergroupapplication.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class GroupPostModel(
+        @PrimaryKey val id: Int
+)
